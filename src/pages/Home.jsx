34705827163f40a6d43ce43d3f0d6752813a4d81
Sanaullah23 <sanaullahb.019@gmail.com>
2023-12-18 17:04:03 +0500

    My error checked

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useState } from 'react'
+
 import NavBar from '../components/NavBar'
 import HeroSection from '../components/HeroSection'
 
 
 const Home = () => {
-  const [name , setName]=useState("");
+  // const [name , setName]=useState("");
   return (
     <div>
         
